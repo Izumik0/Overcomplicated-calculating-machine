4c8050fc473ef30f71333c090385af890a581f9c
Izumiko <maciek.wiszowaty.mw@gmail.com>
2026-03-07 22:03:49 +0100

    Duzo nowych rzeczy robi

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ Jak używać:
 PLDCz.py - bierze pod uwage wszystkie atomy czyli daje nam informacje zwrotną o wszystkich więzach   
 PLDCzG.py - bierze pod uwage tylko Guanine, która tworzy szkielet G-4   
 jeżeli chodzi o komendę to PLDCzG.py używa się tak samo jak PLDCz.py   
-SMM.py - obliczenia na wielu rdzeniach, szybsza wersja PLDCz.py, która jest zaprojektowana do sprawdzania pojedyńćzych klatek w symulacji (wymaga wczesniejszego rozdziału symulacji na klatki i używamy tutaj folderu z klatkami jako parametr flagi -s)
+SMM.py - obliczenia na wielu rdzeniach, szybsza wersja PLDCz.py, która jest zaprojektowana do sprawdzania pojedyńćzych klatek w symulacji
 
 komenda: python PLDCz.py -s [plik ze strukturą] (musi być w fomacie .pdb) -n [plik z więzami] (musie być w formacie .txt) -o [jak ma nazywać się folder] 
+
+komenda: python SMM.py -p [plik .pdb na podstawie którego robiono symulacje] -t [trajektoria .xtc z symulacji] -w [plik .gro z symulacji] -n [Plik z więzami] -o [output]
 
 Program PLDCz.py generuje zbiorczy raport dla danej struktury, raport o atomach/parach których nie znalazł oraz plik .tcl który umożliwia wizualizacje więzów, które spełniają warunki.
 
--- SMM.py
+++ SMM.py
@@ -1,27 +1,29 @@
-import argparse
-import os
-import sys
+import argparse, subprocess, concurrent.futures
+import os, tracemalloc, sys, time, datetime
 import itertools
 import numpy as np
 import pandas as pd
 import mdtraj
 import glob
 from biopandas.pdb import PandasPdb
-import concurrent.futures
-import time
-import datetime
+import matplotlib.pyplot as plt
+
+
 
 # Argumenty dla konsoli
 parser = argparse.ArgumentParser(
     description='Obliczanie więzów między atomami w strukturach DNA według informacji z NOE')
 
 #utworzenie flag dla programu
-parser.add_argument('-s', '--struct', type=str, required=True, help='Ścieżka do pliku struktury (np. structure.pdb)')
+parser.add_argument('-p', '--top', type=str, required=True, help='Ścieżka do topologii (pojedynczy plik .pdb)')
+parser.add_argument('-t', '--traj', type=str, required=True, help='Ścieżka do trajektorii (.xtc)')
+parser.add_argument('-w', '--tpr', type=str, required=True, help='Ścieszka do pliku .tpr/.gro symulacji (trzeba osuszyć)')
 parser.add_argument('-n', '--noe', type=str, required=True, help='Ścieżka do pliku z danymi NOE (.txt)')
 parser.add_argument('-o', '--out', type=str, default='wyniki_noe', help='Nazwa folderu na wyniki (domyślnie: wyniki_noe)')
 
 args = parser.parse_args()
 
+os.makedirs(args.out, exist_ok=True)
 
 #Słownik do sprawdzania atomów bliźniaczych
 atom_blizniaki = {
@@ -35,6 +37,25 @@ atom_blizniaki = {
 
 }
 
+# Suszarka molekularna - osuszanie wody z trajektorii
+def suszarka(xtc, tpr, out ):
+    base=os.path.basename(xtc).replace('.xtc', '')
+    suchy=os.path.join(out, f"{base}_dry.xtc")
+
+    if os.path.exists(suchy):
+        print(f"Sucha trajektoria już istanieje:{suchy}. Pomijamy suszenie")
+        return suchy
+    print(f"Odpalam GROMACS aby suszyć trajektorie {xtc}")
+
+    komenda= f" echo 1 | gmx trjconv -f {xtc} -s {tpr} -o {suchy}"
+
+    try:
+        subprocess.run(komenda, shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        print(f"Trajektoria jest już sucha i zapisana jako: {suchy}")
+        return suchy
+    except subprocess.CalledProcessError:
+        print("cos nie działa, sprawdz czy jest gromacs lub dobry plik")
+        sys.exit(1)
 
 #szukanie bliźniaków i pobieranie dla nich danych z mapy
 def pobierz_grupe_indeksow(nr_res, nazwa_atom, mapa):
@@ -49,7 +70,6 @@ def pobierz_grupe_indeksow(nr_res, nazwa_atom, mapa):
     if czysty_atom in atom_blizniaki:
         nazwa_blizniaka = atom_blizniaki[czysty_atom]
         klucz_blizniaka = f"{czysty_nr}_{nazwa_blizniaka}"
-
         indeksy_blizniaka = list(mapa.get(klucz_blizniaka, []))
         indeksy.extend(indeksy_blizniaka)
 
@@ -58,13 +78,82 @@ def pobierz_grupe_indeksow(nr_res, nazwa_atom, mapa):
 
 # Analiza pliku pdb - fancy funkcja
 
-def analiza_pdb (sciezka_pdb, linie_noe):
-    try:
-        traj=mdtraj.load(sciezka_pdb)
-        dpdb=PandasPdb().read_pdb(sciezka_pdb).df['ATOM']
-    except wykluczenia as e:
-        print(f"Nie można wczytać {sciezka_pdb}:{e}")
-        return sciezka_pdb, 0, 0, 0, "BŁĄD"
+def analiza_pdb (plik, linie_noe, mapa_indeksow):
+    lista_najlepszych_roz=[]
+
+    for line in linie_noe:
+            nr_N_1 = line.split()[0]
+            atom_1 = line.split()[1]
+            nr_N_2 = line.split()[2]
+            atom_2 = line.split()[3]
+            war_porow_str = line.split()[5]
+
+            #the black box - czyli sobie znajduje, kombinuje i liczy odległości
+            indeks_1 = pobierz_grupe_indeksow(nr_N_1, atom_1, mapa_indeksow)
+            indeks_2 = pobierz_grupe_indeksow(nr_N_2, atom_2, mapa_indeksow)
+
+            # system pomijania pustych par
+            if len(indeks_1) == 0 or len(indeks_2) == 0:
+                continue
+
+            # powrót do black boxa
+            pary_str = list(itertools.product(indeks_1, indeks_2))
+            pary_maciez = np.array(pary_str)
+            pary = pary_maciez.astype(int) - 1  # i znów na int (+ jest tu łatanie duck tape'em)
+            war_porow_f = float(war_porow_str)
+
+            #oblicza odległości na podstawie par które zostały utworzone
+            #odległości są przedstawiane w macierzy gdzie kolumny to te same atomy_1 z rórnych czasetek a wiersze to tak samo ale atomy_2
+            odleglosci = mdtraj.compute_distances(plik, pary)  # tu jest odległość w nm
+            odleglosci_a = odleglosci * 10  #odległości w arstrongach czy jak się to nazywa
+
+            for i, (indeks_1, indeks_2) in enumerate(pary):
+                srednie_dla_par = np.mean(odleglosci_a, axis=0)
+                roznice_wszystkich = srednie_dla_par - war_porow_f
+                #Do vmd ogarnianie par
+                ind_win = np.argmin(roznice_wszystkich)
+                naj_exp=roznice_wszystkich[ind_win]
+
+
+            # Dodajemy ją do głównej listy statystyk (do raportu końcowego)
+            lista_najlepszych_roz.append(naj_exp)
+
+    arr_diff = np.array(lista_najlepszych_roz)
+    ilosc=len(arr_diff)
+
+    limit_granicy=0.5
+    if ilosc > 0:
+        count_ok = np.sum(arr_diff <= 0)
+        count_borderline = np.sum((arr_diff > 0) & (arr_diff <= limit_granicy))
+        count_bad = np.sum(arr_diff > limit_granicy)
+        proc_ok=np.round((count_ok/ilosc)*100, 2)
+        proc_borderline=np.round((count_borderline/ilosc)*100, 2)
+        proc_bad=np.round((count_bad/ilosc)*100, 2)
+    else:
+        # Jeśli program nie znalazł żadnych więzów (ilosc == 0), wpisujemy same zera
+        count_ok = 0.0
+        count_borderline = 0.0
+        count_bad = 0.0
+
+    return count_ok, proc_ok, count_borderline, proc_borderline, count_bad, proc_bad, "OK"
+
+# Glowny mech dzialania skryptu
+if __name__ == "__main__":
+
+    # 1. Start rejestru RAM - for debug purpuse
+    tracemalloc.start()
+
+    # 2. Wczytywanie NOE
+    print(f"Wczytuję plik NOE: {args.noe} ...")
+    with open(args.noe, 'r') as f:
+        linie_noe = [l.strip() for l in f.readlines() if l.strip()]
+
+
+    # Wczytanie trajektorii .xtc
+    print (f"Ładowanie trajektorii {args.traj} na podstawie {args.top}")
+    sucha_traj=suszarka(args.traj, args.tpr, args.out)
+    traj = mdtraj.load(sucha_traj, top=args.top)
+    dpdb = PandasPdb().read_pdb(args.top).df['ATOM']
 
     # Mapa poprawek ozmaczen atomow
     mapa_poprawek = {
@@ -91,132 +180,67 @@ def analiza_pdb (sciezka_pdb, linie_noe):
         if klucz not in mapa_indeksow:
             mapa_indeksow[klucz] = []
 
-        # Pamiętaj o przesunięciu o -1, jeśli mdtraj liczy od 0, a PDB od 1!
-        indeks_mdtraj = int(row['atom_number'])
-        mapa_indeksow[klucz].append(indeks_mdtraj)
+        mapa_indeksow[klucz].append(row['atom_number'])
 
-    lista_najlepszych_roz = []
-
-    for line in linie_noe:
-        nr_N_1 = line.split()[0]
-        atom_1 = line.split()[1]
-        nr_N_2 = line.split()[2]
-        atom_2 = line.split()[3]
-        war_porow_str = line.split()[5]
-
-        #the black box - czyli sobie znajduje, kombinuje i liczy odległości
-        indeks_1 = pobierz_grupe_indeksow(nr_N_1, atom_1, mapa_indeksow)
-        indeks_2 = pobierz_grupe_indeksow(nr_N_2, atom_2, mapa_indeksow)
-
-        # system pomijania pustych par
-        if len(indeks_1) == 0 or len(indeks_2) == 0:
-            continue
-
-        # powrót do black boxa
-        pary_str = list(itertools.product(indeks_1, indeks_2))
-        pary_maciez = np.array(pary_str)
-        pary = pary_maciez.astype(int) - 1  # i znów na int (+ jest tu łatanie duck tape'em)
-        war_porow_f = float(war_porow_str)
-
-        #oblicza odległości na podstawie par które zostały utworzone
-        #odległości są przedstawiane w macierzy gdzie kolumny to te same atomy_1 z rórnych czasetek a wiersze to tak samo ale atomy_2
-        odleglosci = mdtraj.compute_distances(traj, pary)  # tu jest odległość w nm
-        odleglosci_a = odleglosci * 10  #odległości w arstrongach czy jak się to nazywa
-
-        for i, (indeks_1, indeks_2) in enumerate(pary):
-            srednie_dla_par = np.mean(odleglosci_a, axis=0)
-            roznice_wszystkich = srednie_dla_par - war_porow_f
-            #Do vmd ogarnianie par
-            ind_win = np.argmin(roznice_wszystkich)
-            naj_exp=roznice_wszystkich[ind_win]
-
-
-        # Dodajemy ją do głównej listy statystyk (do raportu końcowego)
-        lista_najlepszych_roz.append(naj_exp)
-
-    arr_diff = np.array(lista_najlepszych_roz)
-    ilosc=len(arr_diff)
-
-    limit_granicy=0.5
-    if ilosc > 0:
-        count_ok = np.sum(arr_diff <= 0)
-        count_borderline = np.sum((arr_diff > 0) & (arr_diff <= limit_granicy))
-        count_bad = np.sum(arr_diff > limit_granicy)
-        proc_ok=(count_ok/ilosc)*100
-        proc_borderline=(count_borderline/ilosc)*100
-        proc_bad=(count_bad/ilosc)*100
-    else:
-        # Jeśli program nie znalazł żadnych więzów (ilosc == 0), wpisujemy same zera
-        count_ok = 0.0
-        count_borderline = 0.0
-        count_bad = 0.0
-
-    return sciezka_pdb, count_ok, proc_ok, count_borderline, proc_borderline, count_bad, proc_bad, "OK"
-
-# Glowny mech dzialania skryptu
-if __name__ == "__main__":
-
-    # 1. Tu masz swoje wczytywanie argumentów (argparse)
-    folder_pdb = args.struct
-    plik_noe = args.noe
-    folder_out = args.out
-
-    # 2. Wczytywanie NOE
-    print(f"Wczytuję plik NOE: {plik_noe} ...")
-    with open(plik_noe, 'r') as f:
-        linie_noe = [l.strip() for l in f.readlines() if l.strip()]
-
-    # 3. Szukanie plików PDB
-    szukana_sciezka = os.path.join(folder_pdb, "*.pdb")
-    pliki_pdb = glob.glob(szukana_sciezka)
-    print(f"Znaleziono {len(pliki_pdb)} plików PDB do analizy.")
-
-    wyniki_zbiorcze = []
-
-    print(f"🚀 Odpalam masową analizę {len(pliki_pdb)} plików na wielu rdzeniach...")
+    wyniki_nonsort=[]
     start_time = time.time()
-    total_files = len(pliki_pdb)
 
-    # 4. Multiprocessing - jebu jebu na kazdym rdzeniu
-    with concurrent.futures.ProcessPoolExecutor() as executor:
+with concurrent.futures.ProcessPoolExecutor()as executor:
 
-        futures = {executor.submit(analiza_pdb, sciezka, linie_noe): sciezka for sciezka in pliki_pdb}
+    przyszle_wyniki={}
+    for i in range(traj.n_frames):
+        poj_klatka=traj[i]
+        zadanie = executor.submit(analiza_pdb, poj_klatka, linie_noe, mapa_indeksow)
+        przyszle_wyniki[zadanie]=i
 
-        for i, future in enumerate(concurrent.futures.as_completed(futures), 1):
-            sciezka = futures[future]
-            try:
-                wynik = future.result()
-                wyniki_zbiorcze.append({
-                    'Plik': os.path.basename(wynik[0]),
-                    'OK': wynik[1],
-                    '%OK': wynik[2],
-                    'Na_granicy': wynik[3],
-                    '%Na_granicy': wynik[4],
-                    'Zle': wynik[5],
-                    "%Zle": wynik[6],
-                    'Status': wynik[7]
-                })
+    zrobione=0
+    for zadanie in concurrent.futures.as_completed(przyszle_wyniki):
+        numer_klatki=przyszle_wyniki[zadanie]
+        wynik=zadanie.result()
+        wyniki_nonsort.append((numer_klatki, wynik))
+        zrobione += 1
 
+        # --- MAGIA ETA ---
+        elapsed_time = time.time() - start_time  # Ile czasu minęło od startu
+        avg_time_per_file = elapsed_time / zrobione  # Średni czas na jeden plik
+        files_left = traj.n_frames - zrobione  # Ile plików zostało
+        eta_seconds = int(avg_time_per_file * files_left)  # Przewidywany czas w sekundach
 
-                # --- MAGIA ETA ---
-                elapsed_time = time.time() - start_time  # Ile czasu minęło od startu
-                avg_time_per_file = elapsed_time / i  # Średni czas na jeden plik
-                files_left = total_files - i  # Ile plików zostało
-                eta_seconds = int(avg_time_per_file * files_left)  # Przewidywany czas w sekundach
+        current, peak = tracemalloc.get_traced_memory()
 
-                # Formatujemy sekundy do ładnego HH:MM:SS
-                eta_str = str(datetime.timedelta(seconds=eta_seconds))
+        # Formatujemy sekundy do ładnego HH:MM:SS
+        eta_str = str(datetime.timedelta(seconds=eta_seconds))
 
-                print(f"[{i}/{total_files}] Przeanalizowano: {os.path.basename(sciezka)} | ETA: {eta_str}")
+        print(f"[{zrobione}/{traj.n_frames}] | ETA: {eta_str} | Obecne zużycie RAM[MB]: {current/10**6:.2f} | Szczytowe zużycie RAM[MB]: {peak/10**6:.2f}")
 
-            except Exception as exc:  # <--- TUTAJ POPRAWIONA LITERÓWKA (Exception)
-                print(f"❌ Plik {os.path.basename(sciezka)} wygenerował błąd: {exc}")
+    wyniki_sort=sorted(wyniki_nonsort, key=lambda x: x[0])
+    wyniki_zbiorcze=[]
 
-    os.makedirs(folder_out, exist_ok=True)
+    for numer_klatki, wynik in wyniki_sort:
+        wyniki_zbiorcze.append({
+            'Plik': f"Klatka_{numer_klatki + 1}",
+            'OK': wynik[0],
+            '%OK': wynik[1],
+            'Na_granicy': wynik[2],
+            '%Na_granicy': wynik[3],
+            'Zle': wynik[4],
+            "%Zle": wynik[5],
+            'Status': wynik[6]
+        })
+
     df_wyniki = pd.DataFrame(wyniki_zbiorcze)
-    sciezka_raportu = os.path.join(folder_out, "ZBIORCZY_RAPORT_NOE.csv")
+    sciezka_raportu = os.path.join(args.out, "ZBIORCZY_RAPORT_NOE.csv")
     df_wyniki.to_csv(sciezka_raportu, sep=';', index=False)
 
+    #hISTOGRAM
+    plt.figure(figsize=[10, 10])
+    plt.hist(df_wyniki['%OK'], bins=20, color='green', edgecolor='black')
+    plt.title('Rozkład zgodności')
+    plt.xlabel('Procent zgodności więzów')
+    plt.ylabel('Liczba klatek')
+    sciezka_wykresu=os.path.join(args.out, "histogram.png")
+    plt.savefig(sciezka_wykresu, dpi=300, bbox_inches='tight' )
+    plt.close()
     print(f"\n✅ Zakończono! Zapisano zbiorczy raport: {sciezka_raportu}")
 
 
